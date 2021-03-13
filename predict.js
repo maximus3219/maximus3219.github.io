12ded4deb1061f5fd63a7faf137ad74c71a6d3e3
maximus3219 <77224119+maximus3219@users.noreply.github.com>
2021-03-13 23:59:57 +0800

    Update predict.js

--- predict.js
+++ predict.js
@@ -34,7 +34,7 @@ $("#predict-button").click(async function () {
 	// Pre-process the image
 	console.log( "Loading image..." );
 	let tensor = tf.browser.fromPixels(image, 3)
-		.resizeNearestNeighbor([224, 224]) // change the image size
+		.resizeNearestNeighbor([299, 299]) // change the image size
 		.expandDims()
 		.toFloat()
 		// RGB -> BGR
