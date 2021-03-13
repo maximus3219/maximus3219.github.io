88f4902aa2540e6b3433b488bc01c44ba7aebad3
maximus3219 <77224119+maximus3219@users.noreply.github.com>
2021-03-13 13:13:34 +0800

    Update predict.js

--- predict.js
+++ predict.js
@@ -19,7 +19,7 @@ $( document ).ready(async function () {
 	modelLoaded = false;
 	$('.progress-bar').show();
     console.log( "Loading model..." );
-    model = await tf.loadLayersModel('Flower_model_VGG17/model.json');
+    model = await tf.loadLayersModel('myxoid_model/model.json');
     console.log( "Model loaded." );
 	$('.progress-bar').hide();
 	modelLoaded = true;
