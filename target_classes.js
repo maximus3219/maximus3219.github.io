290286934c8406c131f578bfda37b810a6fd5f13
maximus3219 <77224119+maximus3219@users.noreply.github.com>
2021-03-13 13:19:55 +0800

    Update target_classes.js

--- target_classes.js
+++ target_classes.js
@@ -1,7 +1,4 @@
 TARGET_CLASSES = {
-  0: "Extraskeletal myxoid chondrosarcoma",
-  1: "Intramuscular myxoma",
-  2: "Low grade fibromyxoid sarcoma",
-  3: "Myxofibrosarcoma",
-  4: "Myxoid liposarcoma"
+  0: "Intramuscular myxoma",
+  1: "Myxofibrosarcoma"
 };
